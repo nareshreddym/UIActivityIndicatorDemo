760ba20973e51673aa6d0c20eba867b76a3c2a6a
Naresh Reddy M <nareshreddym@outlook.com>
2016-03-14 17:34:03 +0530

    Added code to show UIActivityIndicatorView
    Added code to show loading bar on application's Window

---
+++ MNViewController.m
@@ -0,0 +1,38 @@
+
+@implementation ViewController
+-(UIActivityIndicatorView *)getSpinner
+{
+    static UIActivityIndicatorView *spinnerView = nil;
+    if(!spinnerView)
+    {
+        spinnerView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        [spinnerView setFrame:(CGRect){0,0,CGRectGetWidth([[[[UIApplication sharedApplication] delegate] window] frame]),CGRectGetHeight([[[[UIApplication sharedApplication] delegate] window] frame])}];
+        [spinnerView setCenter:[[[UIApplication sharedApplication] delegate] window].center];
+    }
+    return spinnerView;
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [[self getSpinner] startAnimating];
+    [[self getSpinner] setBackgroundColor:[[UIColor lightGrayColor] colorWithAlphaComponent:0.5]];
+    [[self getSpinner] setColor:[UIColor blueColor]];
+    [[[[UIApplication sharedApplication] delegate] window] addSubview:[self getSpinner]];
+    dispatch_async(dispatch_get_main_queue(), ^
+                   {
+                       [self performSelector:@selector(hideLoadingBar:) withObject:[self getSpinner] afterDelay:5.0];
+                   });
+}
+-(void)hideLoadingBar:(UIActivityIndicatorView *)loadingBar
+{
+    [loadingBar stopAnimating];
+    [loadingBar removeFromSuperview];
+    loadingBar = nil;
+}
+-(void)hideLoadingBar:(UIActivityIndicatorView *)loadingBar
+{
+    [loadingBar stopAnimating];
+    [loadingBar removeFromSuperview];
+    loadingBar = nil;
+}
+@end
